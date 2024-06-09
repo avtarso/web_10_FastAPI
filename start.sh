c75507bd3104bd8cbe438c388ae009217d8d8aca
Avtarso <t0676352927@gmail.com>
2024-06-10 01:14:02 +0300

    fix start.sh

--- start.sh
+++ start.sh
@@ -3,5 +3,5 @@ alembic revision --autogenerate -m "Create table contacts"
 alembic upgrade head
 python seed.py
 kill -9 $(lsof -t -i:8000)
-uvicorn main_simple:app
+uvicorn main:app --host 0.0.0.0 --port 8000 --reload
 
