bd8ef0bc645f2c5dcce68899af731d34ae9a69db
Avtarso <t0676352927@gmail.com>
2024-06-10 01:27:41 +0300

    add main_simple start

---
+++ start_simple.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+alembic revision --autogenerate -m "Create table contacts"
+alembic upgrade head
+python seed.py
+kill -9 $(lsof -t -i:8000)
+uvicorn main_simple:app --host 0.0.0.0 --port 8000 --reload
+
